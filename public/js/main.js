4c7dc0bbcf1f2e3ce07ec72540608af704ac3d04
unknown <spbaisden@gmail.com>
2024-04-04 17:23:36 -0500

    Undo refresh changes

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,5 @@ import {updateMarkers} from './map.js';
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Application initialized");
     initializeMap();
-    setInterval(updateMarkers, 15000); // 5000 ms = 5 seconds
 });
 
