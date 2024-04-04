3143e2d5292adde035655179a08b36f4a1f21012
unknown <spbaisden@gmail.com>
2024-04-03 20:32:17 -0500

    Deploy

--- public/js/dataService.js
+++ public/js/dataService.js
@@ -1,5 +1,5 @@
 export async function fetchAircraftData() {
-    const response = await fetch('http://localhost:3000/aircraft');
+    const response = await fetch('https://skyscouter.onrender.com//aircraft');
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
@@ -7,7 +7,7 @@ export async function fetchAircraftData() {
 }
 
 export async function fetchISSPosition() {
-    const response = await fetch('http://localhost:3000/issPosition');
+    const response = await fetch('https://skyscouter.onrender.com//issPosition');
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
