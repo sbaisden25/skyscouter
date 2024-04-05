275d129760205374aa8317f05cecbae0ed7964f3
unknown <spbaisden@gmail.com>
2024-04-05 14:30:04 -0500

    Identify more aircraft/CSS improvement

--- public/js/icons.js
+++ public/js/icons.js
@@ -32,9 +32,10 @@ export const plottedAsPlaneIcon = ['C17', 'B737', 'C30J', 'DH8D', 'C27J', 'A321'
     'B734', 'G120', 'A148', 'A319', 'M28', 'C650', 'A124', 'CL60', 'L410', 'F900', 'F28', 'G222', 'C25C',
     'E120', 'EXPL', 'TUCA', 'E35L', 'RJ1H', 'CL2T', 'LJ45', 'GLF6', 'E145', 'W135', 'C172', 'DA42', 'Y12',
     'R135', 'BE9L', 'E3TF', 'GLF4', 'B732', 'D328', 'B763', 'H25A', 'BL8', 'P3', 'CN35', 'C212', 'DC3T',
-    'B752', 'SW4', 'AT75', 'P46T', 'LJ35', 'DH8A', 'DA40', 'E737', 'B190', 'FA7X', 'E3CF', 'GL5T',
-    'G12T', 'C208', 'GLEX', 'AT45', 'T154', 'IL16', 'P180', 'A21N', 'A359', 'AN30', 'IL96', 'AN28',
-    'E2', 'E550', 'DH8C', 'BE40', 'B742', 'E190', 'FA20', 'G280', 'C2', 'A310', 'DHC6', 'AT72', 'C56X'];
+    'B752', 'SW4', 'AT75', 'P46T', 'LJ35', 'DH8A', 'DA40', 'E737', 'B190', 'FA7X', 'E3CF', 'GL5T', 'E110',
+    'G12T', 'C208', 'GLEX', 'AT45', 'T154', 'IL16', 'P180', 'A21N', 'A359', 'AN30', 'IL96', 'AN28', 'C680',
+    'E2', 'E550', 'DH8C', 'BE40', 'B742', 'E190', 'FA20', 'G280', 'C2', 'A310', 'DHC6', 'AT72', 'C56X', 
+    'T206', 'DH8B', 'C680', 'B744'];
 
 export const plottedAsHelicopterIcon = ['H64', 'H60', 'EC45', 'EC35', 'H53S', 'B407', 'B212', 'S92',
     'EH10', 'B412', 'A169', 'B505', 'A109', 'B429', 'A139', 'NH90', 'SURN', 'W3', 'AS50', 'AS65', 'B06', 
@@ -44,7 +45,7 @@ export const plottedAsHelicopterIcon = ['H64', 'H60', 'EC45', 'EC35', 'H53S', 'B
 export const plottedAsUAVIcon = ['Q4'];
 
 export const plottedAsFighterJetIcon = ['FA50', 'T38', 'HAWK', 'E314', 'EUFI', 'F16', 'M346', 'F100', 'A149',
-    'M339'];
+    'M339', 'HAR'];
 
 export const doNotPlot = ['PC5T', 'GLID', 'AT8T', 'PC6T', 'PC12', 'TBM7', 'F260', 'PC7', 'PC21', 'BE20',
     'C560', 'PC9', 'SIRA', 'ULAC', 'Z42', 'G115', 'TBM9', 'TEX2', 'T34T', 'AT3T', 'R44', 'ASTR', 'B722', 
